363cdfa2df72e839a6955188c8b21627661ad8a7
BimaARV <bimaptra993@gmail.com>
2026-06-22 09:44:39 +0700

    add alert submit success feedback

--- resources/views/booking-list.blade.php
+++ resources/views/booking-list.blade.php
@@ -1,6 +1,19 @@
 @extends('layouts.public')
 @section('title', 'Cek Booking Saya - Techub')
 @section('content')
+@if(session('success'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        {{ session('success') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+    </div>
+@endif
+
+@if(session('error'))
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        {{ session('error') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+    </div>
+@endif
 
 <div class="container py-5">
     <div class="row justify-content-center">
